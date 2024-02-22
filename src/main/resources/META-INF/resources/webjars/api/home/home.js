243cf5a7e593e86af822554b27b1b69bd7e02af6
Fabrice Daugan <fabrice.daugan@gmail.com>
2024-02-23 00:00:20 +0100

    Add 'x-api-via-user' header in Swagger

--- src/main/resources/META-INF/resources/webjars/api/home/home.js
+++ src/main/resources/META-INF/resources/webjars/api/home/home.js
@@ -1,11 +1,28 @@
 /*
  * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
  */
-define({
+define(["../../../rest/swagger-ui-bundle.js","../../../rest/swagger-ui-standalone-preset.js"], function(SwaggerUIBundle,SwaggerUIStandalonePreset) {
+    return {
 	initialize: function () {
-		$('.table').dataTable({
-			dom: 't',
-			pageLength: -1
-		});
+	    window.SwaggerTranslator;
+        $(function(){
+          // Build a system
+          const ui = SwaggerUIBundle({
+            url: "rest/openapi.json",
+            dom_id: '#swagger-ui',
+            defaultModelsExpandDepth: -1,
+            deepLinking: false,
+            presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIStandalonePreset
+            ],
+            plugins: [
+              SwaggerUIBundle.plugins.DownloadUrl
+            ],
+            layout: "StandaloneLayout"
+          })
+          window.ui = ui
+        })
 	}
+	};
 });
